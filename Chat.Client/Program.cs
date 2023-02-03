3f842ae4d4ad797bf2e3a20cf3114058576159ac
Hubert J <hubert.janiuk.2ti@gmail.com>
2023-02-03 23:13:01 +0100

    Add project files.

---
+++ Chat.Client/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+namespace Chat.Client
+{
+	internal class Program
+	{
+		static async Task Main(string[] args)
+		{
+			var connection = new HubConnectionBuilder()
+				.WithUrl("http://localhost:5000/hubs/chat")
+				.Build();
+
+			await connection.StartAsync();
+
+			
+			Console.WriteLine("Podaj swoją nazwę użytkownika: ");
+			string cUsername = Console.ReadLine();
+			Console.WriteLine($"Witaj {cUsername}! Możesz rozpocząć swoją konwersację z innymi użytkownikami czatu!");
+
+
+			
+
+			connection.On<string, string>("ShowMessage", (username, message) =>
+			{
+				Console.WriteLine($"{username}: {message}");
+			});
+
+			while (true)
+			{
+				string CMessage = Console.ReadLine();
+				await connection.SendAsync("SendMessage", cUsername, CMessage);
+			}
+		}
+	}
+}
---
+++ Chat.Server/Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Chat.Server.Hubs
+{
+	public class ChatHub : Hub
+	{
+		public async Task SendMessage(string username, string message)
+		{ 
+			await Clients.All.SendAsync("ShowMessage", username, message);
+		}
+	}
+}
